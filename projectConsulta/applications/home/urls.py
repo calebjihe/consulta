c22320dda76000095b48b60b5c86f11bb3063136
VISION <calebjihek09@gmail.com>
2023-06-27 18:51:27 -0600

    primer avance de vista de detail

--- projectConsulta/applications/home/urls.py
+++ projectConsulta/applications/home/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 app_name="inicio"
+
 urlpatterns = [
     path('app1/', views.Inicio.as_view()),
     path('app2/', views.Inicio2.as_view()),
     path('app3/', views.MunicipiosConsListView.as_view()),
-    path('app4/<pk>/', views.PresidenteDetailView.as_view()),
+    path('presidente/<pk>/', views.PresidenteDetailView.as_view(), name="presidente"),
 ]
